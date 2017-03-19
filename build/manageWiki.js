cdc6e45ab7940f96fba08609ecbe151c9a4957a8
TevinLi <6317595@qq.com>
2017-03-19 12:23:54 +0800

    梳理自动更新导航模块与图片拖拽

--- build/manageWiki.js
+++ build/manageWiki.js
@@ -25,6 +25,7 @@ module.exports = {
          * id：文库id，由真实地址计算而来，也就是说文库是基于不同地址来管理的
          * root：文库根目录地址
          * path：文库文档地址
+         * treeMD5：文件清单md5值，用于检测文库目录树变化
          * deprecated：弃用标记，当文库被删除或转移走后，原有地址标记为弃用
          */
         this._wikis[id] = {
@@ -36,7 +37,7 @@ module.exports = {
     },
     //创建文库id
     createWikiId: function (root) {
-        root = root.replace(/\\/g, '/');
+        root = root.replace(/\\/g, '/').replace(/\/$/, '');
         //累加地址字符串每个字符Unicode值与此字符序号的乘积
         let code = 0;
         for (let j = 0; j < root.length; j++) {
@@ -59,13 +60,12 @@ module.exports = {
         }
     },
     //检查文库是否仍然有效，失效则标记弃用
-    checkWikiValid: function (path) {
-        let root = path.split('library')[0];
-        //当文件夹不存在或者文件夹不为amWiki时，标记弃用
-        //从atom目录树删除项目，并不会标记弃用，必须删除本地文库文件和文件夹
-        if (!fs.existsSync(path) || !mngFolder.isAmWiki(path)) {
-            let id = this.createWikiId(root);
-            if (typeof this._wikis[id] !== 'undefined') {
+    checkWikiValid: function (id) {
+        if (typeof this._wikis[id] !== 'undefined') {
+            let root = this._wikis[id];
+            //当文件夹不存在或者文件夹不为amWiki时，标记弃用
+            //从atom目录树删除项目，并不会标记弃用，必须删除文库本地文件和文件夹
+            if (!fs.existsSync(root) || !mngFolder.isAmWiki(root)) {
                 this._wikis[id].deprecated = true;
             }
         }
